e1741b08145c35cb721776cb7d07c135a54520f7
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-04-11 22:46:57 -0400

    Modeling Testing
    Testing models

--- brad_notebook.py
+++ brad_notebook.py
@@ -6,6 +6,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
 
 #------------------------------------------------------
 # Read in csv file
@@ -14,6 +19,58 @@ import seaborn as sns
 nv_df = pd.read_csv('nonvoters_data.csv')
 # print(nv_df.shape)
 # print(nv_df.columns)
+
+#------------------------------------------------------
+# Preprocessing
+#------------------------------------------------------
+
+nv_df.columns = ['RespId', 'weight',
+              'q1_uscitizen',
+              'q2_important_voting','q2_important_jury','q2_important_following','q2_important_displaying','q2_important_census',
+              'q2_important_pledge','q2_important_military','q2_important_respect','q2_important_god','q2_important_protesting',
+              'q3_statement_racism1`','q3_statement_racism2','q3_statement_feminine',
+              'q3_statement_msm','q3_statement_politiciansdontcare','q3_statement_besensitive',
+              'q4_impact_officialsfed','q4_impact_officialsstate','q4_impact_officialslocal',
+              'q4_impact_news','q4_impact_wallstreet','q4_impact_lawenforcement',
+              'q5_electionmatters',
+              'q6_officialsarelikeyou',
+              'q7_governmentdesign',
+              'q8_trust_presidency','q8_trust_congress','q8_trust_supremecourt','q8_trust_cdc','q8_trust_electedofficials',
+              'q8_trust_fbicia','q8_trust_newsmedia','q8_trust_police','q8_trust_postalservice',
+              'q9_politicalsystems_democracy','q9_politicalsystems_experts','q9_politicalsystems_strongleader','q9_politicalsystems_army',
+              'q10_disability','q10_chronic_illness','q10_unemployed','q10_evicted',
+              'q11_lostjob','q11_gotcovid','q11_familycovid',
+              'q11_coviddeath','q11_worriedmoney','q11_quitjob',
+              'q14_view_of_republicans',
+              'q15_view_of_democrats',
+              'q16_how_easy_vote',
+              'q17_secure_votingmachines','q17_secure_paperballotsinperson','q17_secure_paperballotsmail','q17_secure_electronicvotesonline',
+              'q18_votingsituations1','q18_votingsituations2','q18_votingsituations3','q18_votingsituations4','q18_votingsituations5',
+              'q18_votingsituations6','q18_votingsituations7','q18_votingsituations8','q18_votingsituations9','q18_votingsituations10',
+              'q19_get_more_voting1','q19_get_more_voting2','q19_get_more_voting3','q19_get_more_voting4','q19_get_more_voting5',
+              'q19_get_more_voting6','q19_get_more_voting7','q19_get_more_voting8','q19_get_more_voting9','q19_get_more_voting10',
+              'q20_currentlyregistered',
+              'q21_plan_to_vote',
+              'q22_whynotvoting_2020',
+              'q23_which_candidate_supporting',
+              'q24_preferred_voting_method',
+              'q25_howcloselyfollowing_election',
+              'q26_which_voting_category',
+              'q27_didyouvotein18','q27_didyouvotein16','q27_didyouvotein14',
+              'q27_didyouvotein12','q27_didyouvotein10','q27_didyouvotein08',
+              'q28_whydidyouvote_past1','q28_whydidyouvote_past2','q28_whydidyouvote_past3','q28_whydidyouvote_past4',
+              'q28_whydidyouvote_past5','q28_whydidyouvote_past6','q28_whydidyouvote_past7','q28_whydidyouvote_past8',
+              'q29_whydidyounotvote_past1','q29_whydidyounotvote_past2','q29_whydidyounotvote_past3','q29_whydidyounotvote_past4','q29_whydidyounotvote_past5',
+              'q29_whydidyounotvote_past6','q29_whydidyounotvote_past7','q29_whydidyounotvote_past8','q29_whydidyounotvote_past9','q29_whydidyounotvote_past10',
+              'q30_partyidentification',
+              'q31_republicantype',
+              'q32_democratictype',
+              'q33_closertowhichparty',
+              'ppage', 'educ', 'race', 'gender', 'income_cat', 'voter_category'
+              ]
+
+nv_df['Age_Group'] = pd.cut(nv_df['ppage'], bins = age_bins, labels = age_labels_cut, right = False)
+
 
 #------------------------------------------------------
 # Race Pie Chart
@@ -56,7 +113,6 @@ plt.show()
 
 age_labels_cut = ['twenties', 'thirties', 'forties', 'fifties', 'sixties', 'elderly']
 age_bins= [20, 30, 40, 50, 60, 70, 200]
-nv_df['Age_Group'] = pd.cut(nv_df['ppage'], bins = age_bins, labels = age_labels_cut, right = False)
 total_age = nv_df['Age_Group'].count()
 twenties = nv_df[nv_df['Age_Group'] == 'twenties']['Age_Group'].count()/total_age
 thirties = nv_df[nv_df['Age_Group'] == 'thirties']['Age_Group'].count()/total_age
@@ -114,6 +170,33 @@ plt.show()
 # Correlation Matrix
 #------------------------------------------------------
 
-corrMatrix = nv_df.corr()
-sns.heatmap(corrMatrix)
+# corrMatrix = nv_df.corr()
+# sns.heatmap(corrMatrix)
+# plt.show()
+
+#------------------------------------------------------
+# Modeling
+#------------------------------------------------------
+
+le = LabelEncoder()
+nv_df['educ'] = le.fit_transform(nv_df['educ'])
+nv_df['race'] = le.fit_transform(nv_df['race'])
+nv_df['gender'] = le.fit_transform(nv_df['gender'])
+nv_df['income_cat'] = le.fit_transform(nv_df['income_cat'])
+nv_df['voter_category'] = le.fit_transform(nv_df['voter_category'])
+nv_df['Age_Group'] = le.fit_transform(nv_df['Age_Group'])
+
+nv_df_mod = nv_df[(nv_df['q23_which_candidate_supporting'] == 1) | (nv_df['q23_which_candidate_supporting'] == 2)]
+X = nv_df_mod.drop('q23_which_candidate_supporting', axis=1)
+y = nv_df_mod['q23_which_candidate_supporting']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 100)
+clf = RandomForestClassifier(n_estimators=100)
+sel = SelectFromModel(clf)
+sel.fit(X_train, y_train)
+importances = clf.feature_importances_
+f_importances = pd.Series(importances, nv_df_mod.iloc[:, 1:].columns)
+f_importances.sort_values(ascending=False, inplace=True)
+f_importances.plot(x='Features', y='Importance', kind='bar', figsize=(16, 9), rot=90, fontsize=15)
+plt.tight_layout()
 plt.show()
